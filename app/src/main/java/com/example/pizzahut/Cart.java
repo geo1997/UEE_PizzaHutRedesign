e849f29a7c77e4f6c831fd355a2c35fed9bed429
Priyan Abeyasekare <pabeyasekare@gmail.com>
2020-10-04 17:40:01 +0530

    final1

---
+++ app/src/main/java/com/example/pizzahut/Cart.java
@@ -0,0 +1,55 @@
+package com.example.pizzahut;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.pizzahut.adapter.CartAdapter;
+import com.example.pizzahut.model.CartItem;
+import com.example.pizzahut.ui.Checkout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class Cart extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    ArrayList<CartItem> cartItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        recyclerView = findViewById(R.id.cart_items_recyclerview);
+
+        cartItems = new ArrayList<>();
+        cartItems.add(new CartItem("BBQ Chicken Pizza","Rs.1560",R.drawable.bbq_chicken));
+        cartItems.add(new CartItem("Smoked Chicken Pizza","Rs.1200",R.drawable.smoked_chicken));
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        RecyclerView.LayoutManager layoutManager1 = layoutManager;
+
+        recyclerView.setLayoutManager(layoutManager1);
+
+        CartAdapter adapter = new CartAdapter(this,cartItems);
+
+        recyclerView.setAdapter(adapter);
+
+        Button btncont = (Button)findViewById(R.id.cart_continue_btn);
+        btncont.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Cart.this, Checkout.class));
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/pizzahut/FoodDetails.java
+++ app/src/main/java/com/example/pizzahut/FoodDetails.java
@@ -1,7 +1,33 @@
 package com.example.pizzahut;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class FoodDetails extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.example.pizzahut.adapter.CartAdapter;
+import com.example.pizzahut.ui.Checkout;
+
+public class FoodDetails extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+        setContentView(R.layout.activity_food_details);
+
+        Button btncont = (Button)findViewById(R.id.cart_btn);
+        btncont.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(FoodDetails.this, MainActivity.class));
+                Toast.makeText(FoodDetails.this, "Item added to cart", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 }
