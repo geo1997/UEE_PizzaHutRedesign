f3e9182643a6a6b899a32b122f9abb4b34a8674c
ASUS <thumalee@outlook.com>
2020-10-06 22:33:47 +0530

    Feedback and update forms

---
+++ app/src/main/java/com/example/pizzahut/Feedback.java
@@ -0,0 +1,26 @@
+package com.example.pizzahut;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Feedback extends AppCompatActivity {
+
+    Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+
+        spinner = findViewById(R.id.inqSpinner);
+       // spinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+    }
+}
